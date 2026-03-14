24e890ac93532630730214aeec2eec729e069ae3
Kaustabh Ganguly <stabgan@users.noreply.github.com>
2026-03-14 16:18:53 +0530

    Reapply "fix: modernize PyTorch code + rewrite README"
    This reverts commit 9f9b57597cc0ef45b0b506b9af8e6844c4cbb3ce.

--- lstm.py
+++ lstm.py
@@ -2,18 +2,20 @@ import torch
 import torch.nn as nn
 import torchvision.transforms as transforms
 import torchvision.datasets as dsets
-from torch.autograd import Variable
+
+# Device configuration
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 '''
 STEP 1: LOADING DATASET
 '''
-train_dataset = dsets.MNIST(root='./data', 
-                            train=True, 
+train_dataset = dsets.MNIST(root='./data',
+                            train=True,
                             transform=transforms.ToTensor(),
                             download=True)
 
-test_dataset = dsets.MNIST(root='./data', 
-                           train=False, 
+test_dataset = dsets.MNIST(root='./data',
+                           train=False,
                            transform=transforms.ToTensor())
 
 '''
@@ -25,12 +27,12 @@ n_iters = 3000
 num_epochs = n_iters / (len(train_dataset) / batch_size)
 num_epochs = int(num_epochs)
 
-train_loader = torch.utils.data.DataLoader(dataset=train_dataset, 
-                                           batch_size=batch_size, 
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                           batch_size=batch_size,
                                            shuffle=True)
 
-test_loader = torch.utils.data.DataLoader(dataset=test_dataset, 
-                                          batch_size=batch_size, 
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                          batch_size=batch_size,
                                           shuffle=False)
 
 '''
@@ -42,42 +44,32 @@ class LSTMModel(nn.Module):
         super(LSTMModel, self).__init__()
         # Hidden dimensions
         self.hidden_dim = hidden_dim
-        
+
         # Number of hidden layers
         self.layer_dim = layer_dim
-        
+
         # Building your LSTM
         # batch_first=True causes input/output tensors to be of shape
         # (batch_dim, seq_dim, feature_dim)
         self.lstm = nn.LSTM(input_dim, hidden_dim, layer_dim, batch_first=True)
-        
+
         # Readout layer
         self.fc = nn.Linear(hidden_dim, output_dim)
-    
+
     def forward(self, x):
         # Initialize hidden state with zeros
-        #######################
-        #  USE GPU FOR MODEL  #
-        #######################
+        h0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim).to(x.device)
 
-        if torch.cuda.is_available():
-            h0 = Variable(torch.zeros(self.layer_dim, x.size(0), self.hidden_dim).cuda())
-        else:
-            h0 = Variable(torch.zeros(self.layer_dim, x.size(0), self.hidden_dim))
-        
         # Initialize cell state
-        if torch.cuda.is_available():
-            c0 = Variable(torch.zeros(self.layer_dim, x.size(0), self.hidden_dim).cuda())
-        else:
-            c0 = Variable(torch.zeros(self.layer_dim, x.size(0), self.hidden_dim))
-        
+        c0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim).to(x.device)
+
         # One time step
-        out, (hn, cn) = self.lstm(x, (h0,c0))
-        
+        out, (hn, cn) = self.lstm(x, (h0, c0))
+
         # Index hidden state of last time step
         # out.size() --> 100, 28, 100
-        # out[:, -1, :] --> 100, 100 --> just want last time step hidden states! 
-        out = self.fc(out[:, -1, :]) 
+        # out[:, -1, :] --> 100, 100 --> just want last time step hidden states!
+        out = self.fc(out[:, -1, :])
         # out.size() --> 100, 10
         return out
 
@@ -90,14 +82,8 @@ layer_dim = 3  # ONLY CHANGE IS HERE FROM ONE LAYER TO TWO LAYER
 output_dim = 10
 
 model = LSTMModel(input_dim, hidden_dim, layer_dim, output_dim)
+model.to(device)
 
-#######################
-#  USE GPU FOR MODEL  #
-#######################
-
-if torch.cuda.is_available():
-    model.cuda()
-    
 '''
 STEP 5: INSTANTIATE LOSS CLASS
 '''
@@ -108,80 +94,67 @@ STEP 6: INSTANTIATE OPTIMIZER CLASS
 '''
 learning_rate = 0.1
 
-optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)  
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
 '''
 STEP 7: TRAIN THE MODEL
 '''
 
 # Number of steps to unroll
-seq_dim = 28  
+seq_dim = 28
 
-iter = 0
+iter_count = 0
 for epoch in range(num_epochs):
+    model.train()
     for i, (images, labels) in enumerate(train_loader):
-        # Load images as Variable
-        #######################
-        #  USE GPU FOR MODEL  #
-        #######################
-        if torch.cuda.is_available():
-            images = Variable(images.view(-1, seq_dim, input_dim).cuda())
-            labels = Variable(labels.cuda())
-        else:
-            images = Variable(images.view(-1, seq_dim, input_dim))
-            labels = Variable(labels)
-            
+        # Load images as tensors to device
+        images = images.view(-1, seq_dim, input_dim).to(device)
+        labels = labels.to(device)
+
         # Clear gradients w.r.t. parameters
         optimizer.zero_grad()
-        
+
         # Forward pass to get output/logits
         # outputs.size() --> 100, 10
         outputs = model(images)
-        
+
         # Calculate Loss: softmax --> cross entropy loss
         loss = criterion(outputs, labels)
-        
+
         # Getting gradients w.r.t. parameters
         loss.backward()
-        
+
         # Updating parameters
         optimizer.step()
-        
-        iter += 1
-        
-        if iter % 500 == 0:
-            # Calculate Accuracy         
+
+        iter_count += 1
+
+        if iter_count % 500 == 0:
+            # Calculate Accuracy
             correct = 0
             total = 0
-            # Iterate through test dataset
-            for images, labels in test_loader:
-                #######################
-                #  USE GPU FOR MODEL  #
-                #######################
-                if torch.cuda.is_available():
-                    images = Variable(images.view(-1, seq_dim, input_dim).cuda())
-                else:
-                    images = Variable(images.view(-1, seq_dim, input_dim))
-                
-                # Forward pass only to get logits/output
-                outputs = model(images)
-                
-                # Get predictions from the maximum value
-                _, predicted = torch.max(outputs.data, 1)
-                
-                # Total number of labels
-                total += labels.size(0)
-                
-                # Total correct predictions
-                #######################
-                #  USE GPU FOR MODEL  #
-                #######################
-                if torch.cuda.is_available():
-                    correct += (predicted.cpu() == labels.cpu()).sum()
-                else:
-                    correct += (predicted == labels).sum()
-            
+
+            model.eval()
+            with torch.no_grad():
+                # Iterate through test dataset
+                for images, labels in test_loader:
+                    images = images.view(-1, seq_dim, input_dim).to(device)
+                    labels = labels.to(device)
+
+                    # Forward pass only to get logits/output
+                    outputs = model(images)
+
+                    # Get predictions from the maximum value
+                    _, predicted = torch.max(outputs.data, 1)
+
+                    # Total number of labels
+                    total += labels.size(0)
+
+                    # Total correct predictions
+                    correct += (predicted == labels).sum().item()
+
             accuracy = 100 * correct / total
-            
+
             # Print Loss
-            print('Iteration: {}. Loss: {}. Accuracy: {}'.format(iter, loss.data[0], accuracy))
+            print('Iteration: {}. Loss: {:.4f}. Accuracy: {:.2f}%'.format(
+                iter_count, loss.item(), accuracy))
